c81d8c248a07a7c460fe4d937a10a1411b2133ad
BrMrx <bruno@marx.cologne>
2025-04-27 19:12:12 +0200

    Update module.php

--- FreeAtHomeDevice/module.php
+++ FreeAtHomeDevice/module.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 require_once __DIR__ . '/../libs/ColorHelper.php';
+require_once __DIR__ . '/../libs/FunctionID.php';
+require_once __DIR__ . '/../libs/PairingID.php';
 
 class FreeAtHomeDevice extends IPSModule
 {
