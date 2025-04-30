ca7046b12de30ce2ed16e8b9bf1e25aa912f0cc5
BrMrx <bruno@marx.cologne>
2025-04-30 13:48:59 +0200

    Update module.php

--- FreeAtHomeDevice/module.php
+++ FreeAtHomeDevice/module.php
@@ -194,6 +194,16 @@ class FreeAtHomeDevice extends IPSModule
         {
             $lRequestArray = explode('.',$lRequest);
 
+            if( !isset($lDevices->{$lRequestArray[0]}) )
+            {
+                $this->SendDebug(__FUNCTION__, 'device not found', 0);
+                return;
+            }
+            if( !isset($lRequestArray[0]}->channels->{$lRequestArray[1]}) )
+            {
+                $this->SendDebug(__FUNCTION__, 'channel not found', 0);
+                return;
+            }
             $lValue = $lDevices->{$lRequestArray[0]}->channels->{$lRequestArray[1]}->outputs->{$lRequestArray[2]}->value;
 
             $lDataObj[$lRequestArray[0]][$lRequestArray[1]][$lRequestArray[2]] = $lValue;
